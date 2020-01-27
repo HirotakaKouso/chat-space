7bcdf7c5a869c8f216d065f5c200b3257b41603e
HirotakaKouso <kousoda7@yahoo.co.jp>
2020-01-27 16:19:37 +0900

    aa

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '<用意したElastic IP>', user: 'ec2-user', roles: %w{app db web}
+server '54.250.44.43', user: 'ec2-user', roles: %w{app db web}
 
 
 
